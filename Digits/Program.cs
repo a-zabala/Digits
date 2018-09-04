38ac31417f9b346ac8c83a7dee409b447ed1b4eb
Angelina Zabala <angekr@aol.com>
2018-09-03 22:32:40 -0500

    initial

--- Digits/Program.cs
+++ Digits/Program.cs
@@ -6,7 +6,18 @@ namespace Digits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number:");
+            string result = Console.ReadLine();
+            
+            Console.WriteLine("Your number {0} has {1} digits", result, Digits(result));
+            Console.ReadLine();
+        }
+        private static int Digits(string result)
+        {
+            result = result.Replace(",", "");
+            result = result.Replace(".", "");
+            return result.Length;
+
         }
     }
 }
