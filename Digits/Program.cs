97d6d0d4a5d0f6b8282644951566954542277fbf
Angelina Zabala <angekr@aol.com>
2018-09-03 23:01:30 -0500

    added a random integer to find the number of digits

--- Digits/Program.cs
+++ Digits/Program.cs
@@ -10,14 +10,26 @@ namespace Digits
             string result = Console.ReadLine();
             
             Console.WriteLine("Your number {0} has {1} digits", result, Digits(result));
+            Console.WriteLine();
+            Console.WriteLine("We can also find the length of a random integer.");
+            Random rnd = new Random();
+            int chosen = rnd.Next(1, 99999999);
+            Console.Write("{0:N}", chosen);
+            Console.WriteLine(" has " + Digits(chosen) + " digits.");
             Console.ReadLine();
+
+            
         }
         private static int Digits(string result)
         {
             result = result.Replace(",", "");
-            result = result.Replace(".", "");
             return result.Length;
 
+        }
+        private static int Digits(int chosen)
+        {
+            string number = chosen.ToString();
+            return number.Length;
         }
     }
 }
